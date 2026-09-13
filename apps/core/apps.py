ade354e63099d1d774a4b73e0b7365cd001120e9
Aasish <gk.aasishkarki@gmail.com>
2026-09-13 08:52:06 +0545

    Django Setup

---
+++ apps/core/apps.py
@@ -0,0 +1,6 @@
+# apps/core/apps.py
+from django.apps import AppConfig
+
+class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.core'  # <-- Change from 'core' to 'apps.core'
